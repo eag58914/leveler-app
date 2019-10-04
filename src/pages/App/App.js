c237a1a0cb5587283d42491c1bf32937acd9aa45
Elco Garcia <garciaelco18@gmail.com>
2019-10-04 16:08:32 -0500

    moved app.js file

---
+++ src/pages/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from '../../components/NavBar/NavBar';
+import LoginPage from '../LoginPage/LoginPage';
+import HomePage from '../HomePage/HomePage';
+
+function App() {
+	return (
+		<div className="App">
+			<HomePage />
+		</div>
+	);
+}
+
+export default App;
