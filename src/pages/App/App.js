5c80a1e95459120d887783304436558bf3bda09e
Elco Garcia <garciaelco18@gmail.com>
2019-10-04 17:23:55 -0500

    added routes for sign up and login forms

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -2,11 +2,17 @@ import React from 'react';
 import NavBar from '../../components/NavBar/NavBar';
 import LoginPage from '../LoginPage/LoginPage';
 import HomePage from '../HomePage/HomePage';
+import { Link, Route, Switch } from 'react-router-dom';
+import SignupPage from './SignupPage/SignupPage';
 
 function App() {
 	return (
 		<div className="App">
 			<HomePage />
+			<Switch>
+				<Route exact path="/login" render={(props) => <LoginPage {...props} />} />
+				<Route exact path="/signup" render={(props) => <SignupPage {...props} />} />
+			</Switch>
 		</div>
 	);
 }
